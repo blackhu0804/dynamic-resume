eb88c0a3555849603de4170d5925f59a1d063cea
hu970804 <812510003@qq.com>
2018-01-27 13:07:28 +0800

    add

--- js/main.js
+++ js/main.js
@@ -52,26 +52,8 @@ html{
 }
 `
 
-const showStyles = (num, callback) => {
-  let n = 0;
-  let id = setInterval(() => {
-    if (n >= result.length) {
-      window.clearInterval(id);
-      callback && callback();
-    } else {
-      text.innerHTML = result.substring(0, n);
-      text.innerHTML = Prism.highlight(text.innerHTML, Prism.languages.css);
-    
-      cssCode.innerHTML = result.substring(0, n);
-      text.scrollTop = 1000;
-      n += 1;
-    }
-  }, 10)
-}
-
-
-var resumeText = 
-`
+var resumeText =
+  `
 # Black
 ----
 
@@ -93,22 +75,119 @@ var resumeText =
 * [我的博客](http://www.blackhu.site/)
 `
 
+var result2 =
+  `
+  /* 
+  你好，我是Black。
+  我将以讲义的动画介绍我自己，只用文字太单调了。
+*/
+
+/* 白色背景太单调了，首先我们改下背景 */
+html {
+  background: #ccc;
+  font-size: 16px;
+}
+/* 下面我们加个边框 */
+#text {
+  border: 1px solid red;
+}
+/* 下面将代码高亮 */
+.token.comment{color:slategray;}
+.token.selector{color: #690;}
+.token.punctuation {color: #999;}
+.token.property {color: #905;} 
+/* 加点3D效果怎么样。Let‘s begin！ */
+html{
+  perspective: 1000px;
+  -webkit-perspective: 1000px;
+}
+#text {
+  position: fixed; 
+  left: 1rem; 
+  top: 0; 
+  transition: .5;
+  -webkit-transition: .5; 
+  transform: rotateY(10deg) translateZ(-100px);
+  -webkit-transform: rotateY(10deg) translateZ(-100px);
+}
+/* 接下来我们准备一个编辑器，正式介绍下自己。 */
+.resume-wrap{
+  width: 48vw; 
+  height: 90vh; 
+  position: fixed; 
+  right: .5rem; 
+  top: 0;
+  padding: .5em;
+  border: 1px solid;
+  background: white; 
+  color: #222;
+  overflow: auto;
+  transition: .5;
+  -webkit-transition: .5; 
+  transform: rotateY(-10deg) translateZ(-100px);
+  -webkit-transform: rotateY(-10deg) translateZ(-100px);
+}
+/* 这种格式太难看了，我们让他转换成markdown格式吧O(∩_∩)O哈哈~ */
+`
+
+const showStyles2 = (callback) => {
+  let n = result.length;
+  let id = setInterval(() => {
+    if (n >= result2.length) {
+      window.clearInterval(id);
+      callback && callback();
+    } else {
+      text.innerHTML = result2.substring(0, n);
+      text.innerHTML = Prism.highlight(text.innerHTML, Prism.languages.css);
+
+      cssCode.innerHTML = result2.substring(0, n);
+      text.scrollTop = 1000;
+      n += 1;
+    }
+  }, 100)
+}
+
+const showStyles = (callback) => {
+  let n = 0;
+  let id = setInterval(() => {
+    if (n >= result.length) {
+      window.clearInterval(id);
+      callback && callback();
+    } else {
+      text.innerHTML = result.substring(0, n);
+      text.innerHTML = Prism.highlight(text.innerHTML, Prism.languages.css);
+
+      cssCode.innerHTML = result.substring(0, n);
+      text.scrollTop = 1000;
+      n += 1;
+    }
+  }, 100)
+}
+
+
 const showResume = (callback) => {
   let n = 0;
-  let timer = setInterval( () => {
-    if(n >= resumeText.length){
+  let timer = setInterval(() => {
+    if (n >= resumeText.length) {
       clearInterval(timer);
       callback && callback();
     } else {
       resume.innerHTML = resumeText.substring(0, n);
-      resume.scrollTop = 1000; 
+      resume.scrollTop = 1000;
       n += 1;
     }
   }, 10);
 }
 
-showStyles(0, () => {
-  showResume( () => {
-    //showStyles(1)
+const markdownToHtml = (callback) => {
+  let resumeTextMD = marked(resumeText);
+  resume.innerHTML = resumeTextMD;
+}
+
+showStyles(() => {
+  showResume(() => {
+    showStyles2( () => {
+      markdownToHtml()
+    })
   })
 })
--- js/prism.js
+++ js/prism.js
@@ -1,5 +1,5 @@
 /* PrismJS 1.10.0
-http://prismjs.com/download.html?themes=prism&languages=markup+css+clike+javascript */
+http://prismjs.com/download.html?themes=prism&languages=markup+css+clike+javascript+markdown */
 var _self = (typeof window !== 'undefined')
 	? window   // if in browser
 	: (
@@ -733,3 +733,123 @@ if (Prism.languages.markup) {
 
 Prism.languages.js = Prism.languages.javascript;
 
+Prism.languages.markdown = Prism.languages.extend('markup', {});
+Prism.languages.insertBefore('markdown', 'prolog', {
+	'blockquote': {
+		// > ...
+		pattern: /^>(?:[\t ]*>)*/m,
+		alias: 'punctuation'
+	},
+	'code': [
+		{
+			// Prefixed by 4 spaces or 1 tab
+			pattern: /^(?: {4}|\t).+/m,
+			alias: 'keyword'
+		},
+		{
+			// `code`
+			// ``code``
+			pattern: /``.+?``|`[^`\n]+`/,
+			alias: 'keyword'
+		}
+	],
+	'title': [
+		{
+			// title 1
+			// =======
+
+			// title 2
+			// -------
+			pattern: /\w+.*(?:\r?\n|\r)(?:==+|--+)/,
+			alias: 'important',
+			inside: {
+				punctuation: /==+$|--+$/
+			}
+		},
+		{
+			// # title 1
+			// ###### title 6
+			pattern: /(^\s*)#+.+/m,
+			lookbehind: true,
+			alias: 'important',
+			inside: {
+				punctuation: /^#+|#+$/
+			}
+		}
+	],
+	'hr': {
+		// ***
+		// ---
+		// * * *
+		// -----------
+		pattern: /(^\s*)([*-])(?:[\t ]*\2){2,}(?=\s*$)/m,
+		lookbehind: true,
+		alias: 'punctuation'
+	},
+	'list': {
+		// * item
+		// + item
+		// - item
+		// 1. item
+		pattern: /(^\s*)(?:[*+-]|\d+\.)(?=[\t ].)/m,
+		lookbehind: true,
+		alias: 'punctuation'
+	},
+	'url-reference': {
+		// [id]: http://example.com "Optional title"
+		// [id]: http://example.com 'Optional title'
+		// [id]: http://example.com (Optional title)
+		// [id]: <http://example.com> "Optional title"
+		pattern: /!?\[[^\]]+\]:[\t ]+(?:\S+|<(?:\\.|[^>\\])+>)(?:[\t ]+(?:"(?:\\.|[^"\\])*"|'(?:\\.|[^'\\])*'|\((?:\\.|[^)\\])*\)))?/,
+		inside: {
+			'variable': {
+				pattern: /^(!?\[)[^\]]+/,
+				lookbehind: true
+			},
+			'string': /(?:"(?:\\.|[^"\\])*"|'(?:\\.|[^'\\])*'|\((?:\\.|[^)\\])*\))$/,
+			'punctuation': /^[\[\]!:]|[<>]/
+		},
+		alias: 'url'
+	},
+	'bold': {
+		// **strong**
+		// __strong__
+
+		// Allow only one line break
+		pattern: /(^|[^\\])(\*\*|__)(?:(?:\r?\n|\r)(?!\r?\n|\r)|.)+?\2/,
+		lookbehind: true,
+		inside: {
+			'punctuation': /^\*\*|^__|\*\*$|__$/
+		}
+	},
+	'italic': {
+		// *em*
+		// _em_
+
+		// Allow only one line break
+		pattern: /(^|[^\\])([*_])(?:(?:\r?\n|\r)(?!\r?\n|\r)|.)+?\2/,
+		lookbehind: true,
+		inside: {
+			'punctuation': /^[*_]|[*_]$/
+		}
+	},
+	'url': {
+		// [example](http://example.com "Optional title")
+		// [example] [id]
+		pattern: /!?\[[^\]]+\](?:\([^\s)]+(?:[\t ]+"(?:\\.|[^"\\])*")?\)| ?\[[^\]\n]*\])/,
+		inside: {
+			'variable': {
+				pattern: /(!?\[)[^\]]+(?=\]$)/,
+				lookbehind: true
+			},
+			'string': {
+				pattern: /"(?:\\.|[^"\\])*"(?=\)$)/
+			}
+		}
+	}
+});
+
+Prism.languages.markdown['bold'].inside['url'] = Prism.util.clone(Prism.languages.markdown['url']);
+Prism.languages.markdown['italic'].inside['url'] = Prism.util.clone(Prism.languages.markdown['url']);
+Prism.languages.markdown['bold'].inside['italic'] = Prism.util.clone(Prism.languages.markdown['italic']);
+Prism.languages.markdown['italic'].inside['bold'] = Prism.util.clone(Prism.languages.markdown['bold']);
